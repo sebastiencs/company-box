41227951356a10f6e7b2cbc3cc839acd84a951b9
Sebastien Chapuis <sebastien@chapu.is>
2020-04-29 10:39:30 +0200

    Handle when company-common is nil

--- company-box.el
+++ company-box.el
@@ -456,7 +456,7 @@ It doesn't nothing if a font icon is used."
           (color (company-box--get-color backend))
           ((c-color a-color i-color s-color) (company-box--resolve-colors color))
           (icon-string (and company-box--with-icons-p (company-box--add-icon candidate)))
-          (candidate-string (concat (propertize company-common 'face 'company-tooltip-common)
+          (candidate-string (concat (and company-common (propertize company-common 'face 'company-tooltip-common))
                                     (substring (propertize candidate 'face 'company-box-candidate) (length company-common) nil)))
           (align-string (when annotation
                           (concat " " (and company-tooltip-align-annotations
